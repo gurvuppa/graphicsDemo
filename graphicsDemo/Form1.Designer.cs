743dd0e922acf8525479c0e79e048cd0fccb7120
Gurvir Uppal <gurviruppal@icloud.com>
2021-05-31 11:50:10 -0400

    Add project files.

---
+++ graphicsDemo/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace graphicsDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.printPreviewControl1 = new System.Windows.Forms.PrintPreviewControl();
+            this.SuspendLayout();
+            // 
+            // printPreviewControl1
+            // 
+            this.printPreviewControl1.Location = new System.Drawing.Point(797, 440);
+            this.printPreviewControl1.Name = "printPreviewControl1";
+            this.printPreviewControl1.Size = new System.Drawing.Size(8, 10);
+            this.printPreviewControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.printPreviewControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PrintPreviewControl printPreviewControl1;
+    }
+}
+
---
+++ graphicsDemo/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace graphicsDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            this.Text = "Graphics Examples";
+
+            Graphics g = this.CreateGraphics();
+            Pen bluePen = new Pen(Color.Blue, 5);
+            SolidBrush blueBrush = new SolidBrush(Color.MediumBlue);
+            SolidBrush yellowBrush = new SolidBrush(Color.Gold);
+            Font drawFont = new Font("IMPACT", 60, FontStyle.Bold);
+            Font tmFont = new Font("Noto", 10, FontStyle.Bold);
+
+            g.Clear(Color.White);
+
+            g.DrawRectangle(bluePen, 30, 30, 300, 100);
+            g.FillRectangle(blueBrush, 30, 30, 300, 100);
+
+            g.DrawEllipse(bluePen, 35, 35, 290, 90);
+            g.FillEllipse(yellowBrush, 35, 35, 290, 90);
+
+            g.DrawString("I", drawFont, blueBrush, 80, 33);
+            g.DrawString("K", drawFont, blueBrush, 115, 33);
+            g.DrawString("E", drawFont, blueBrush, 170, 33);
+            g.DrawString("A", drawFont, blueBrush, 215, 33);
+
+            g.TranslateTransform(290, 110);
+            g.RotateTransform(-30);
+            g.DrawString("T", tmFont, yellowBrush, 0, 0);
+
+            g.TranslateTransform(7, 0);
+            g.RotateTransform(0);
+            g.DrawString("M", tmFont, yellowBrush, 0, 0);
+        }
+    }
+}
